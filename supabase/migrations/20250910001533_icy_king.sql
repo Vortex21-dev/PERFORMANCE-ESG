a6382f4e5eed66ac95033a9dbee8ba9ae8682707
Vortex21-dev <154286034+Vortex21-dev@users.noreply.github.com>
2025-09-10 00:15:54 +0000

    Add sectors and subsectors from provided list

---
+++ supabase/migrations/20250910001533_icy_king.sql
@@ -0,0 +1,26 @@
+/*
+  # Add sectors and subsectors from provided list
+
+  1. New Sectors
+    - Agriculture, Elevage, Pêche et aquaculture, Foresterie, Extraction, Industrie
+    - Bâtiment et Travaux Publics, Services financiers et assurances, Commerce distribution
+    - Transports et logistique, Immobilier, Communication Informatiques et numérique
+    - Hôtellerie et restauration, Education, Santé et action sociale, Administration publique
+    - Agroalimentaire, Énergie, Textiles, Sidérurgie et métallurgie, Chimie et pharmacie
+    - BTP – Bâtiment et travaux publics, Services financiers, Commerce, Télécommunications
+    - Informatique et numérique, Éducation
+
+  2. New Subsectors
+    - Oléagineux, Sucre, Caoutchouc naturel, Café cacao, Riz céréales Vivrier Fruits et agrumes
+    - Volaille, Bovins, Porcins, Pêche et aquaculture, Production de bois
+    - Papier cartons et emballages industriels, Pétrole Gaz naturel Métaux et de minéraux
+    - Agroalimentaire, Energie, Textiles, Sidérurgie et métallurgie, Chimie et pharmacie
+    - Cosmétiques, Engrais et pesticides, Automobile, Aéronautique, Ferroviaire, Plasturgie
+    - Immobilier et infrastructures, Banque Assurances Bourse et gestion de patrimoine
+    - Commerces, Distribution, Aviation Maritime Ferroviaire Routier, Agences immobilières
+    - Développement de logiciels, Services numériques, Télécommunications, Hôtellerie
+    - Restauration, Écoles et universités, Formation professionnelle, Hôpitaux
+    - Services sociaux, Fonction publique, Collectivités territoriales, Organismes gouvernementaux
+
+  3. Sector-Subsector Relationships
+    - All subsectors properly linked to their parent sectors
